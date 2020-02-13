e53b6f7972d93fb580ccbc48c6e7d5268339b3a0
shehabalqudiry <omran.shehab40@gmail.com>
2020-02-13 10:43:21 +0200

    .

---
+++ components/firebase.js
@@ -0,0 +1,16 @@
+import firebase from "firebase";
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: "AIzaSyCFW6TYzIlU8IICik9ka3-LPzLjvmYusMM",
+	authDomain: "auth-f187c.firebaseapp.com",
+	databaseURL: "https://auth-f187c.firebaseio.com",
+	projectId: "auth-f187c",
+	storageBucket: "auth-f187c.appspot.com",
+	messagingSenderId: "980776232410",
+	appId: "1:980776232410:web:698cb340dbf02198c90ce3",
+	measurementId: "G-HP6BKVF58Y"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
